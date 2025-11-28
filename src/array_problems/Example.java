4bd2f97281c22e0c56a339551a3c0f69df55fb7a
Jasurbek <ruzimovjasur2105@gmail.com>
2025-11-28 16:03:08 +0500

    All codes were pushed

---
+++ src/array_problems/Example.java
@@ -0,0 +1,23 @@
+package array_problems;
+
+import java.util.Arrays;
+
+public class Example {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"}; //1-usul
+
+        int[] numbers = new int[8];
+        numbers[0] = 1;  // 2-usul
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+        numbers[5] = 6;
+        numbers[6] = 7;
+        numbers[7] = 8;
+
+        System.out.println(numbers[0] + numbers[1] + numbers[2]);  //6
+
+        System.out.print(Arrays.toString(cars));
+    }
+}
