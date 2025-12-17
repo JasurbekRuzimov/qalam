430152c5e059d40883ca476a9658486576cf050a
Jasurbek <ruzimovjasur2105@gmail.com>
2025-12-17 15:23:37 +0500

    added smth

--- src/array_problems/Example.java
+++ src/array_problems/Example.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class Example {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"}; //1-usul
-
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda", " "}; //1-usul
+        System.out.println(Arrays.toString(cars));
+        cars[1] = "Mercedes Benz";
         int[] numbers = new int[8];
         numbers[0] = 1;  // 2-usul
         numbers[1] = 2;
@@ -18,6 +19,6 @@ public class Example {
 
         System.out.println(numbers[0] + numbers[1] + numbers[2]);  //6
 
-        System.out.print(Arrays.toString(cars));
+        System.out.println(Arrays.toString(cars));
     }
 }
