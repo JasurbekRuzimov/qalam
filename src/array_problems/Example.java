06682bbe1a461fdd49f9a55e3978e6e03c66ecf2
Jasurbek <ruzimovjasur2105@gmail.com>
2025-12-17 17:22:06 +0500

    added smth

--- src/array_problems/Example.java
+++ src/array_problems/Example.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class Example {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda", " "}; //1-usul
-        System.out.println(Arrays.toString(cars));
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda", "Tico"}; //1-usul
+      //  System.out.println(Arrays.toString(cars));
         cars[1] = "Mercedes Benz";
+        int sum = 1;
         int[] numbers = new int[8];
         numbers[0] = 1;  // 2-usul
         numbers[1] = 2;
@@ -17,8 +18,15 @@ public class Example {
         numbers[6] = 7;
         numbers[7] = 8;
 
-        System.out.println(numbers[0] + numbers[1] + numbers[2]);  //6
+//        for (int i = 0; i < numbers.length; i++) {
+//           sum = i*i;
+//           System.out.println(sum);
+//        }
 
-        System.out.println(Arrays.toString(cars));
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println("ID " + i + "   Model " + cars[i]);
+        }
+    //    System.out.println(numbers[0] + numbers[1] + numbers[2]);  //6
+    //    System.out.println(Arrays.toString(cars));
     }
 }
