bb511e816e2a258a4cf579447365a63e04db379d
Jasurbek <ruzimovjasur2105@gmail.com>
2025-12-20 15:46:29 +0500

    All method codes were pushed!

---
+++ src/method_problems/Method.java
@@ -0,0 +1,28 @@
+package method_problems;
+
+public class Method {
+    public static void main(String[] args) {
+        kompyuter15("Lenovo", 2000, 2022);
+        kompyuter16("MSI");
+        kompyuter17("HP Envy 360");
+        kompyuter18("Asus Chromebook");
+    }
+
+    static void kompyuter15(String name, int cost, int date) {
+        System.out.println("15-Kompyuter nomi: " + name + " Narxi: $" + cost + "   yili: " + date);
+    }
+
+    static void kompyuter16(String name) {
+        System.out.println("16-Kompyuter nomi: " + name);
+    }
+
+    static void kompyuter17(String name) {
+        System.out.println("17-Kompyuter nomi: " + name);
+    }
+
+    static void kompyuter18(String name) {
+        System.out.println("18-Kompyuter nomi: " + name);
+    }
+
+
+}
