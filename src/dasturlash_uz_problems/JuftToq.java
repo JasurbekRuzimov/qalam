a0a1ff919c72234d6fcd100c9c95df1ca4ed5b40
Jasurbek <ruzimovjasur2105@gmail.com>
2026-02-27 17:41:03 +0500

    gipotenuza

---
+++ src/dasturlash_uz_problems/JuftToq.java
@@ -0,0 +1,18 @@
+package dasturlash_uz_problems;
+
+public class JuftToq {
+    public static void main(String[] args) {
+        System.out.println(juftToq(8));
+    }
+    public static String juftToq(int n){
+    if (n%2==0) {
+        return "juft";
+    }
+    else {
+        return "toq";
+    }
+
+
+
+    }
+}
