c91a03f630a55e1a7ee4cc54eb5c309238fde468
Jasurbek <ruzimovjasur2105@gmail.com>
2026-02-28 15:42:17 +0500

    km to m

---
+++ src/dasturlash_uz_problems/Kilometr.java
@@ -0,0 +1,10 @@
+package dasturlash_uz_problems;
+
+public class Kilometr {
+    public static void main(String[] args) {
+        System.out.println(km(9));
+    }
+    public static int km(int m){
+        return(m * 1000);
+    }
+}
