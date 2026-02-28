642851cbe591de69881787241efd05d274313665
Jasurbek <ruzimovjasur2105@gmail.com>
2026-02-28 16:04:09 +0500

    added

---
+++ src/dasturlash_uz_problems/MeterSekund.java
@@ -0,0 +1,10 @@
+package dasturlash_uz_problems;
+
+public class MeterSekund {
+    public static void main(String[] args) {
+        System.out.println(m(9));
+    }
+    public static double m(double m){
+        return  (m / 3.6);
+    }
+}
---
+++ src/dasturlash_uz_problems/Selsiy.java
@@ -0,0 +1,10 @@
+package dasturlash_uz_problems;
+
+public class Selsiy {
+    public static void main(String[] args) {
+        System.out.println(celcium(10));
+    }
+    public static double celcium(double c){
+        return (c+273);
+    }
+}
